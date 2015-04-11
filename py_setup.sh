8e7b094c9799a2978dd7c3a337d14ac83e33e9b5
Jim Fasarakis-Hilliard <d.f.hilliard@gmail.com>
2015-04-12 00:35:50 +0300

    Update py_setup.sh

--- py_setup.sh
+++ py_setup.sh
@@ -2,6 +2,7 @@
 
 set -xe 
 
+# Install Dependencies.
 sudo apt-get -yqq update && sudo apt-get -yqq install \
  build-essential \
  libreadline-gplv2-dev \
@@ -11,7 +12,7 @@ sudo apt-get -yqq update && sudo apt-get -yqq install \
  tk-dev libgdbm-dev \
  libc6-dev libbz2-dev
 
-
+# Install python 2.7.9 and 3.4.3
 for PYTHON_VER in 2.7.9 3.4.3; do
 	PYTHON="Python-$PYTHON_VER"
 	wget https://www.python.org/ftp/python/$PYTHON_VER/Python-$PYTHON_VER.tar.xz
@@ -21,7 +22,9 @@ for PYTHON_VER in 2.7.9 3.4.3; do
 	rm -rf $PYTHON
 	if [ $PYTHON_VER = "2.7.9" ]; then
 		python -m ensurepip
+		python -m ensurepip --upgrade
 	else 
 		python3 -m ensurepip
+		python3 -m ensurepip --upgrade
 	fi
 done
